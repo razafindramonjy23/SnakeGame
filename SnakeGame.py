c6e4b427a71bacfdf2ab7a41c7ab28dad7c2850c
Red_Rabbit <159623002+razafindramonjy23@users.noreply.github.com>
2024-02-24 16:50:34 +0000

    Add files via upload

---
+++ SnakeGame.py
@@ -0,0 +1,42 @@
+import pygame
+from pygame.locals import *
+
+def draw_block(): 
+    #surface.fill((110, 110, 5))
+    surface.blit(block, (block_x, block_y))
+    pygame.display.flip()
+
+if __name__ == "__main__": 
+    pygame.init()
+
+    surface = pygame.display.set_mode((500, 500))
+    surface.fill((110, 110, 25))
+
+    block = pygame.image.load(resources/block.png).convert() #Image*********** down block.png
+    block_x = 100
+    block_y = 100
+    surface.blit(block, (block_x, block_y))
+
+    pygame.display.flip()
+
+    running = True
+
+    while running: 
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+
+                    if event.key == K_UP: 
+                        block_y -= 10
+                        draw_block()
+                    if event.key == K_DOWN: 
+                        block_y += 10
+                    if event.key == K_LEFT: 
+                        block_x -= 10
+                    if event.key == K_RIGHT: 
+                        block_x += 10
+
+            elif event.type == QUIT: 
+                running = False
